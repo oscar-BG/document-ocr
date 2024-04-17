6ef9ac2a61c3bf803402157879e71a9dabf5502d
Oscar <oscar04262000@gmail.com>
2024-04-17 17:45:37 -0600

    Refractor CSV, cambiar campos en CSV

--- ocrdocument/views.py
+++ ocrdocument/views.py
@@ -10,7 +10,7 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'media/credencials/ocr-document.j
 from google.cloud import documentai
 from google.api_core.client_options import ClientOptions
 from dotenv import load_dotenv
-
+import json
 load_dotenv()
 
 
@@ -37,7 +37,6 @@ def upload(request):
             instancia.save()
             url_document = instancia.documento.url
             url_format = url_document[1:]
-            print(url_format)
 
             get_text_form_pdf_ocr(url_format)
         return redirect("homepage")
@@ -67,30 +66,36 @@ def get_text_form_pdf_ocr(file_path):
 
         document = response.document
         data = []
+        
+
+        print(f"There are {len(document.pages)} page(s) in this document.")
+        # Convertir el objeto document a un diccionario
+        # serialized_document = documentai.Document.to_dict(document)
+        # Guardar el diccionario como JSON
+        # with open("media/document.json", "w") as json_file:
+            # json.dump(serialized_document, json_file, indent=4)
+        
+        # data = [[entity.mention_text for entity in document.entities if entity.type_ == 'CARD_ID'],
+        #   [entity.mention_text for entity in document.entities if entity.type_ == 'FULL_NAME']]
 
         
+        array_card_id = []
+        array_full_name = []
         for entity in document.entities:
             card_id = ""
-            last_name = ""
-            second_last_name = ""
-            name = ""
+            full_name = ""
 
             if entity.type_ == 'CARD_ID':
                 card_id = entity.mention_text
-            elif entity.type_ == 'LAST_NAME':
-                last_name = entity.mention_text
-            elif entity.type_ == 'SECOND_LAST_NAME':
-                second_last_name = entity.mention_text
-            elif entity.type_ == 'NAME':
-                name = entity.mention_text
-            
-            data.append([card_id.strip(), last_name.strip(), second_last_name.strip(), name.strip()])
+                array_card_id.append(card_id.strip())
+            elif entity.type_ == 'FULL_NAME':
+                full_name = entity.mention_text
+                array_full_name.append(full_name.replace("\n", " "))
 
+            # data.append([card_id.strip(), full_name.strip()])
         
-
-        csv_data = []
-        for row in data:
-            csv_data.append([row[0], row[1], row[2], row[3]])
+        print(array_card_id)
+        print(full_name)
         
         csv_file_name = os.path.splitext(os.path.basename(file_path))[0] + ".csv"
         csv_file_path = os.path.join("media/csv/", csv_file_name)
@@ -103,11 +108,12 @@ def get_text_form_pdf_ocr(file_path):
 
         with open(csv_file_path, mode='w', newline='', encoding='utf-8') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow(['CARD_ID', 'LAST_NAME', 'SECOND_LAST_NAME', 'NAME'])  # Escribir encabezados
-            writer.writerows(csv_data)  # Escribir los datos recolectados
+            writer.writerow(['CARD_ID', 'FULL_NAME'])  # Escribir encabezados
+            for card_id, full_name in zip(array_card_id, array_full_name):
+                writer.writerow([card_id, full_name])
 
         new_csv_instance = SaveFileCsv.objects.create(excel_file=relative_csv_file_path, name_document=csv_file_name)
-
+        
         return True
     except Exception as e:
         print(e)
